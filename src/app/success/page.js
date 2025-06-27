c1bf7c67856ee70674a4d44bb7b73ddbd49b1a24
Steven NAVE <stevennave@gmail.com>
2025-06-27 14:12:37 -0400

    updated success page

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -2,7 +2,7 @@ export default function SuccessPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-teal-50 text-stone-800">
       <h1 className="text-4xl font-bold mb-4">Thank you!</h1>
-      <p>Your booking request has been submitted. We'll be in touch soon.</p>
+      <p>Your booking request has been submitted. We will be in touch soon.</p>
     </div>
   );
 }
